46202a6145740072a612fc9c986c0bf3253bf822
Abhinav Sharma <abhinavsona01@gmail.com>
2025-04-22 19:58:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const socket = require('socket.io');
+const http= require('http');
+const {Chess} = require('chess.js');
+const path = require('path');
+const { title } = require('process');
+
+const app = express();
+
+const server=http.createServer(app);
+const io= socket(server);
+
+const chess = new Chess();
+
+let players = {};
+let currentPlayer = "W";
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res)=>{
+    res.render("index", {title: "Chess Game"});
+});
+
+server.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+});
+
+
+
+
+
+
+
+
+
+
